51f5f0a2dca25d583edb218a2d221ddc11fd09ef
swswsw <apiswswsw@gmail.com>
2019-11-02 13:45:53 -0700

    working trial client code

---
+++ trial/trial_client.js
@@ -0,0 +1,16 @@
+let { connect } = require('lotion')
+
+let GCI = 'a4dfcaffc90a1b21e351bcf6e9f03035d1fc61299dd87d4ad009dd23ffd991db'
+
+
+async function writeState(newdata) {
+  //let { state, send } = await connect(GCI)
+}
+
+(async function () {  
+  // weird.  cannot connect here unless there is a async function before this statement.  very weird.
+  let { state, send } = await connect(GCI)
+  let stateInfo = await state;
+  console.log(stateInfo);
+
+})();
