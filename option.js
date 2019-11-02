2598cfbcc6c8f0d738e9090554ccad5bd46ba418
swswsw <apiswswsw@gmail.com>
2019-11-02 13:14:24 -0700

    working trial node code

--- option.js
+++ option.js
@@ -19,18 +19,30 @@ basic operations
 - buy/sell option
  */
 
- let optionTemplate = {
-   id: undefined, // [integer] 
-   type: undefined, // [string] "call", "put", "butterfly", "barrier"
-   strike: undefined, // [number] strike price
-   expiration: undefined, // [integer]  integer. the block height
-   asset: undefined, // [string] the asset id of the underlying asset
-   owner: undefined, // [string] address of the owner of this option
- }
+let optionTemplate = {
+  id: undefined, // [integer] 
+  type: undefined, // [string] "call", "put", "butterfly", "barrier"
+  strike: undefined, // [number] strike price
+  expiration: undefined, // [integer]  integer. the block height
+  asset: undefined, // [string] the asset id of the underlying asset
+  owner: undefined, // [string] address of the owner of this option
+}
 
-function create() {
+function simpleCreate(type, strike, expiration, asset, owner) {
   // todo: for hackathon, we use timestamp as id.  definitely not an accepetable id.  we'll change that to hash or guid.
+  let option = Object.assign({}, optionTemplate);
+  option.id = new Date().getTime();
+  option.type = type;
+  option.strike = strike;
+  option.expiration = expiration;
+  option.asset = asset;
+  option.owner = owner;
 
+  create(option);
+}
+
+function create(option) {
+  // write to state.
   
 }
 
